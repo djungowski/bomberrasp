28df5ceb483548c0cd24d12e4cd288e1515a003c
Dominik Jungowski <Dominik@Jungowski.de>
2012-11-18 00:18:02 +0100

    draw playfield - looks nice :)

--- lib/field.py
+++ lib/field.py
@@ -16,16 +16,16 @@ class Field:
 		screen_size = screen.get_size()
 		brick_size = self.get_brick_size()
 
-		bricks_per_row = screen_size[0] / brick_size[0]
-		self.logging.debug("Bricks per row: %d" % bricks_per_row)
+		cols = screen_size[0] / brick_size[0]
+		self.logging.debug("Columns: %d" % cols)
 
-		bricks_per_column = screen_size[1] / brick_size[1]
-		self.logging.debug("Bricks per col: %d" % bricks_per_column)
+		rows = screen_size[1] / brick_size[1]
+		self.logging.debug("Rows: %d" % rows)
 
 		upper_rect = pygame.Rect(0, 0, screen_size[0], brick_size[1])
-		lower_rect = pygame.Rect(0, brick_size[1] * (bricks_per_column - 1), screen_size[0], brick_size[1])
-		left_rect = pygame.Rect(0, 0, brick_size[1], brick_size[0] * bricks_per_column)
-		right_rect = pygame.Rect(brick_size[1] * (bricks_per_row - 1), 0, brick_size[1], brick_size[0] * bricks_per_column)
+		lower_rect = pygame.Rect(0, brick_size[1] * (rows - 1), screen_size[0], brick_size[1])
+		left_rect = pygame.Rect(0, 0, brick_size[1], brick_size[0] * rows)
+		right_rect = pygame.Rect(brick_size[1] * (cols - 1), 0, brick_size[1], brick_size[0] * rows)
 
 		self.__bricks.append(upper_rect)
 		self.__bricks.append(lower_rect)
@@ -37,20 +37,35 @@ class Field:
 		self.logging.debug("Left brick rect: %s" % left_rect)
 		self.logging.debug("Right brick rect: %s" % right_rect)
 
+		for row in range(rows):
+			self.logging.debug("Painting row #%d" % row)
+			# First and last line go through
+			if row == 0 or row == rows - 1:
+				for col in range(cols):
+					screen.get_screen().blit(self.__brick, (col * brick_size[0], row * brick_size[1]))
+			# Every other line: brick every 2nd position
+			else:
+				for col in range(cols):
+					if col == 0 or col == cols - 1:
+						screen.get_screen().blit(self.__brick, (col * brick_size[0], row * brick_size[1]))
+					elif col % 2 == 0 and row % 2 == 0:
+							screen.get_screen().blit(self.__brick, (col * brick_size[0], row * brick_size[1]))
+
+		return
 		# Paint upper row
-		for i in range(bricks_per_row):
+		for i in range(cols):
 			screen.get_screen().blit(self.__brick, (i * brick_size[0], 0))
 
 		# Paint bottom row
-		for i in range(bricks_per_row):
-			screen.get_screen().blit(self.__brick, (i * brick_size[0], (bricks_per_column - 1) * brick_size[1]))
+		for i in range(cols):
+			screen.get_screen().blit(self.__brick, (i * brick_size[0], (rows - 1) * brick_size[1]))
 
 		# Paint left side
-		for i in range(bricks_per_column):
+		for i in range(rows):
 			screen.get_screen().blit(self.__brick, (0, i * brick_size[1]))
 
 		# Paint right side
-		for i in range(bricks_per_column):
+		for i in range(rows):
 			screen.get_screen().blit(self.__brick, (screen_size[0] - brick_size[0], i * brick_size[1]))
 
 	def get_bricks(self):
