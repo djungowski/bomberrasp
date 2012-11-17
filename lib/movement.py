1a5256b52f73f055bbb4233e966175452a6377f2
Dominik Jungowski <Dominik@Jungowski.de>
2012-11-17 13:17:24 +0100

    removed animation

--- lib/movement.py
+++ lib/movement.py
@@ -2,9 +2,6 @@
 class Movement:
 	screen = None
 
-	# With how many frames is movement animated
-	__frames = 5
-
 	def __init__(self, screen):
 		self.__screen = screen
 
@@ -27,19 +24,6 @@ class Movement:
 		return new_position
 
 	def move_player(self, player, new_position):
-		old_position = player.get_position()
-		new_position = self.__fix_position(player, new_position)
-
-		movement_x = new_position[0] - old_position[0]
-		movement_x_step = movement_x / self.__frames
-
-		movement_y = new_position[1] - old_position[1]
-		movement_y_step = movement_y / self.__frames
-
-		current_position = list(old_position)
-
-		#for x in range(self.__frames):
-			#current_position[0] = current_position[0] + movement_x_step
-			#current_position[1] = current_position[1] + movement_y_step
+		new_position = self.__fix_position(player, new_position)	
 		self.__screen.move_player(player, new_position)
 		player.set_position(new_position)
